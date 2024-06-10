9933e7258260334ce5e7db49fa68ab7f4294320e
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-06-09 21:46:22 -0500

    Update Suma_digitos.c

--- Suma_digitos.c
+++ Suma_digitos.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int sumaDigitos(int num){
+    int suma = 0;
+    while (num != 0) {
+        suma += num % 10;
+        num /= 10;
+    }
+    return suma;
+}
+
+int main() {
+    int numero;
+    printf("Ingrese un número: ");
+    scanf("%d", &numero);
+    printf("La suma de los dígitos de %d es: %d\n", numero, sumaDigitos(numero));
+    return 0;
+}
