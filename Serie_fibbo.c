cb15c0f87558e51226dd032c1904be7aee3a82b1
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-06-09 21:49:10 -0500

    Update Serie_fibbo.c

--- Serie_fibbo.c
+++ Serie_fibbo.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void mostrarFibonacci(int cantidad) {
+    int a = 0, b = 1, temporal;
+    printf("Los primeros %d números de la serie de Fibonacci son: ", cantidad);
+    for(int i = 0; i < cantidad; i++) {
+        printf("%d ", a);
+        temporal = a;
+        a = b;
+        b = temporal + b;
+    }
+    printf("\n");
+}
+
+int main() {
+    int cantidad;
+    printf("¿Cuántos números de la serie de Fibonacci deseas ver? ");
+    scanf("%d", &cantidad);
+    mostrarFibonacci(cantidad);
+return 0;
+}
