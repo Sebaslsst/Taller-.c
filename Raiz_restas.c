ca0e83b01b90722d1f70c9e59f74a2f80ee34354
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-06-09 21:48:34 -0500

    Update Raiz_restas.c

--- Raiz_restas.c
+++ Raiz_restas.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+float raizCuadrada(int n) {
+    float raiz = 0.0;
+
+    for (int i = 0; i * i <= n; i++) {
+        raiz = i;
+    }
+
+    float precision = 0.001;
+    for (int i = 0; i < 10; i++) {
+        raiz += precision;
+        if (raiz * raiz > n) {
+            raiz -= precision;
+        }
+    }
+
+    return raiz;
+}
+
+int main() {
+    int num;
+    printf("Introduce un número: ");
+    scanf("%d", &num);
+    printf("La raíz cuadrada de %d es: %.3f\n", num, raizCuadrada(num));
+    return 0;
+}
