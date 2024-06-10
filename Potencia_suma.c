31702efe0a919ec4dd801a517bc51473696600a8
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-06-09 21:47:52 -0500

    Update Potencia_suma.c

--- Potencia_suma.c
+++ Potencia_suma.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int potencia(int base, int exponente) {
+    int resultado = 1;
+    for(int i = 0; i < exponente; i++) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
+int main() {
+    int base = 8;
+    int exponente = 3;
+    int resultado = potencia(base, exponente);
+    printf("%d ^ %d = %d\n", base, exponente, resultado);
+    return 0;
+}
