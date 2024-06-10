0d1adae5dcdd5ec6c7985934bb846b6f977fbd22
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-06-09 21:47:17 -0500

    Update Inverso.c

--- Inverso.c
+++ Inverso.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int invertirNumero(int num) {
+    int inverso = 0;
+    for (; num != 0; num /= 10) {
+        inverso = inverso * 10 + num % 10;
+    }
+    return inverso;
+}
+
+int main() {
+    int numero;
+    printf("Por favor ingrese un número: ");
+    scanf("%d", &numero);
+    printf("El inverso del número %d es: %d\n", numero, invertirNumero(numero));
+    return 0;
+}
